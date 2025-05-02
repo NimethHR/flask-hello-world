28ade36093d304674aea5dd313ceff6410aea4a5
NRH <104486228+NimethHR@users.noreply.github.com>
2025-05-03 03:58:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import numpy as np
+from tensorflow.keras.models import load_model
+
 app = Flask(__name__)
+model = load_model('model.h5')
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    input_data = np.array(data['input']).reshape(1, -1)
+    prediction = model.predict(input_data)
+    return jsonify({'prediction': prediction[0].tolist()})
+
+if __name__ == '__main__':
+    app.run()
